d5cc4b33e75f1738b9c790e0e4533ba89e520ef0
Melvin Dinh <mdinh@umich.edu>
2024-11-10 23:48:45 -0500

    final update

--- script/script.js
+++ script/script.js
@@ -4,9 +4,27 @@ document.addEventListener("DOMContentLoaded", () => {
       img.onerror = function () {
         this.onerror = null; // Prevents looping if default image fails
         this.src = "../images/profiles/default.jpg"; // Set the default image
-        this.alt = "broken image";
+        // this.alt = "";
       };
     });
+    document.querySelector("header").setAttribute("tabindex", "0");
+    document.querySelector("header h2").setAttribute("tabindex", "0");
+    document.querySelector("#summary").setAttribute("tabindex", "0");
+    document.querySelector("#summary h2").setAttribute("tabindex", "0");
+    document.querySelector("#summary p").setAttribute("tabindex", "0");
+    document.querySelector("#team-results").setAttribute("tabindex", "0");
+    document.querySelector("#team-results h2").setAttribute("tabindex", "0");
+    document.querySelectorAll("#team-results tr").forEach((row) => {
+        row.setAttribute("tabindex", "0");
+    });
+    document.querySelector("#individual-results").setAttribute("tabindex", "0");
+    document.querySelector("#individual-results h2").setAttribute("tabindex", "0");
+    document.querySelectorAll("#individual-results .athlete").forEach((element) => {
+        element.setAttribute("tabindex", "0");
+    });
+    document.querySelector("#gallery").setAttribute("tabindex", "0");
+    document.querySelector("#gallery h2").setAttribute("tabindex", "0");
+    document.querySelector("footer").setAttribute("tabindex", "0");
 });
 
 //back up if the above code fail to run for some images
@@ -17,3 +35,4 @@ document.addEventListener(
   },
   true
 );
+
